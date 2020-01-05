292c3ebf4487fdf70f0c2a9f7660e7fb469d7cf9
bakanaouji <abeken1224@yahoo.co.jp>
2020-01-06 01:15:52 +0900

    fix game main

--- Game/main.cpp
+++ Game/main.cpp
@@ -2,18 +2,18 @@
 // Created by 阿部 拳之 on 2019/10/29.
 //
 
-#include <array>
 #include <iostream>
 #include <random>
 #include "CFRAgent.hpp"
 #include "Game.hpp"
 #include "Trainer.hpp"
+#include "Trainer.cpp"
 
 int main() {
     std::mt19937 engine((std::random_device()()));
     Kuhn::Game game(engine);
 
-    Trainer::Trainer trainer("cfr");
+    Trainer::Trainer<Kuhn::Game> trainer("cfr");
     float ps[game.playerNum()];
     for (int i = 0; i < game.playerNum(); ++i) {
         ps[i] = 1.0;
@@ -21,6 +21,6 @@ int main() {
     for (int p = 0; p < game.playerNum(); ++p) {
         // game reset
         game.resetForCFR();
-        std::cout << trainer.CFR(game, p, ps, 0) << std::endl;
+        std::cout << trainer.CFR(game, p, 1.0f, 1.0f) << std::endl;
     }
 }
